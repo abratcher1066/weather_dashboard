4cd7a69465dc4d7cd1885fab85ebc9b33e075ccf
Andrew Bratcher <abratcher1066@gmail.com>
2020-08-10 16:59:21 -0400

    wb db +api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const api = {
+  key: "1dc622ad66eda09cce194664d58875ef",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
