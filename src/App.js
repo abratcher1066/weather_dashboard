96b5b418645680cee6ff75ab73a960cfe7318110
Andrew Bratcher <abratcher1066@gmail.com>
2020-08-12 15:00:44 -0400

    City search added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,15 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
         .then(res => res.json())
-        .then(result => setWeather(result));
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        
+        });
+
     }
 
   }
@@ -38,22 +44,26 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-
-        </div>
-
-        <div className="weather-box">
-          <div className="temp">
-            15 C
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>\
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              15 C
+            </div>
+            <div className="weather">Sunny</div>
           </div>
         </div>
-        <div className="weather">Sunny</div>
 
+        ) : ('')}
       </main>
     </div>
   );
