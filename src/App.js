fa1417df96ade7d0ffe18b7b989e1222fdfda149
Andrew Bratcher <abratcher1066@gmail.com>
2020-08-12 14:46:52 -0400

    Fixed api call

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
+        .then(result => setWeather(result));
     }
 
   }
