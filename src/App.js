ee95358ba43804dadc8029498ae267bdd714b213
Andrew Bratcher <abratcher1066@gmail.com>
2020-08-12 14:39:02 -0400

    CSS formatting

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()]
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
           <input 
@@ -15,6 +28,20 @@ function App() {
             placeholder="Search..."
           />
         </div>
+
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">
+            15 C
+          </div>
+        </div>
+        <div className="weather">Sunny</div>
+
       </main>
     </div>
   );
